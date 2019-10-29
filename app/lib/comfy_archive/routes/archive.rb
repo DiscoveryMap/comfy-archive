02d6b19b820c849caf5ff7301929ef85bb0be1c5
Morgan Aldridge <morgant@makkintosshu.com>
2019-10-29 15:20:46 -0400

    Fix of routes for chronologically archived pages by category, plus routes formatting tweaks. Issue #DMSI-605

--- app/lib/comfy_archive/routes/archive.rb
+++ app/lib/comfy_archive/routes/archive.rb
@@ -6,11 +6,11 @@ class ActionDispatch::Routing::Mapper
     scope module: :comfy, as: :comfy do
       namespace :archive, path: options[:path] do
         with_options constraints: { year: %r{\d{4}}, month: %r{\d{1,2}} } do |o|
-          o.get "*cms_path/:year",        to: "index#index", as: :pages_of_year
-          o.get "*cms_path/:year/:month", to: "index#index", as: :pages_of_month
+          o.get "*cms_path/:year",          to: "index#index", as: :pages_of_year
+          o.get "*cms_path/:year/:month",   to: "index#index", as: :pages_of_month
         end
-        get "*cms_path/:category",        to: "index#index", as: :pages_of_category
-        get "(*cms_path)" => "index#index", as: "render_page", action: "/:format"
+        get "*cms_path/category/:category", to: "index#index", as: :pages_of_category
+        get "(*cms_path)",                  to: "index#index", as: :render_page, action: "/:format"
       end
     end
   end
